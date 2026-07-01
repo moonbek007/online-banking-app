b6c08e5c19b3ac50d0f4f7f7ff0949f4267dd5de
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-01 16:52:42 +0500

    feat: Add Sidebar and MobileNavbar to Dashboard

---
+++ components/Sidebar.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { sidebarLinks } from "@/constants"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname()
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 flex cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            alt="Future logo"
+            width={34}
+            height={34}
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Future</h1>
+        </Link>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathName === link.route || pathName.startsWith(`${link.route}`)
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {link.label}
+              </p>
+            </Link>
+          )
+        })}
+
+        {/* USER */}
+      </nav>
+      {/* FOOTER */}
+    </section>
+  )
+}
+
+export default Sidebar
