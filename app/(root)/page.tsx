b330e52ee4c195228841a2de24b7d8453e35944b
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-01 15:21:25 +0500

    feat: Add HeaderBox and other components to Dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,27 @@
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+
 const Dashboard = () => {
+  const loggeIn = { firstName:"Toby"}
   return (
-    <div>Dashboard</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggeIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.50}
+          />
+        </header>
+      </div>
+      </section>
   )
 }
 
